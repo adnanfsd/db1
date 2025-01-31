dd47e85224e38088de04e2e4261ba8e5d99eeab3
adnanfsd <tsadnan39@gmail.com>
2025-01-31 10:30:31 +0530

    updated the code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const studModel = require("./model/student")
 
 var app = express();
 
-var port = 3000;
+var port = 8001;
 app.use(express.json());
 // api to add data to db
 app.post("/",(req,res)=>{
@@ -22,6 +22,28 @@ app.get('/',async(req,res)=>{
     try {
         var data = await studModel.find();
         res.send(data);         
+    } catch (error) {
+        res.send(error);
+    }
+})
+// api to delete data
+
+app.delete('/remove/:id',async(req,res)=>{
+    try {
+        var data = await studModel.findByIdAndDelete(req.params.id,req.body);
+        res.send("deleted successfully");
+    } catch (error) {
+        res.send(error);
+    }
+})
+
+
+// api to update
+
+app.put('/edit/:id',async(req,res)=>{
+    try {
+        var data = await studModel.findByIdAndUpdate(req.params.id,req.body);
+        res.send("updated successfully");
     } catch (error) {
         res.send(error);
     }
